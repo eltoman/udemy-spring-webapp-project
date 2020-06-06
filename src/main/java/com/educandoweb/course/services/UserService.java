4d7a944271ae67be5e4810ec15c1995df1b34a05
eltoman <eltomds@usp.br>
2020-06-06 01:39:45 -0300

    add DatabaseException

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,10 +2,14 @@ package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,13 +33,22 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        Optional<User> user = userRepository.findById(id);
-        userRepository.delete(user.get());
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
-        User userToUpdate = userRepository.getOne(id);
-        userToUpdate.updateData(user);
-        return userRepository.save(userToUpdate);
+        try{
+            User userToUpdate = userRepository.getOne(id);
+            userToUpdate.updateData(user);
+            return userRepository.save(userToUpdate);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 }
